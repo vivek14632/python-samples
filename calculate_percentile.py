e2c3799e405928eb3453db137a6cd666dc7111e9
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2015-11-30 16:35:33 -0500

    Update calculate_percentile.py

--- calculate_percentile.py
+++ calculate_percentile.py
@@ -1,23 +1,19 @@
 #input array
-a=[82,89,94,110,74,122,112,95,100,78,65,60,90,83,87,75,114,85,69,94,124,115,107,88,97,74,72,68,83,91,90,102,77,125,108,65]
+m_a=[82,89,94,110,74,122,112,95,100,78,65,60,90,83,87,75,114,85,69,94,124,115,107,88,97,74,72,68,83,91,90,102,77,125,108,65]
 
 #percentile value, example 25 percentile
-p=.25
+p=.75
 #number of samples
-n=len(a)
+n=len(m_a)
 k=n*p
 #sort the array
-a.sort()
+m_a.sort()
 
 #check if k is an integer
-if(k-int(k) !=0)
-  k=int(k)+1
-  print(a[k-1]
-else
-  ans=(a[int(k)-1] +a[int(k)])/2
-  print(ans)
-  
- 
- 
- 
- 
+if(k-int(k) !=0):
+	k=int(k)+1
+	print(m_a[k-1])
+
+else:
+	ans= ((m_a[int(k)-1] +m_a[int(k)])/2)
+	print(ans)
